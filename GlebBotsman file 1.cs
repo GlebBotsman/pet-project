b2db8e4809f68cd0e6b388f74d26ff17326cc562
GlebBotsman <alekseev.gleb1@mail.ru>
2021-04-26 15:46:48 +0300

    initial commit

---
+++ GlebBotsman file 1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Test_one_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            var userName = Console.ReadLine();
+            Console.WriteLine("You name is"+ userName);
+
+        }
+    }
+}
